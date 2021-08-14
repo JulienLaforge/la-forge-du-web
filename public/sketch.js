9b61c01144e374a916d738a6c1e0fd3952b7cbeb
Julien Laforge <julien.a.laforge@gmail.com>
2021-08-14 16:45:16 +0200

    reforged the index view

--- public/sketch.js
+++ public/sketch.js
@@ -3,27 +3,69 @@ let gap = 25;
 let opacity_top = 255;
 let opacity_bot = 0;
 let isDone = false;
+let lines = [];
+let line1, line2, line3;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 
+  for (let i = 0; i<3; i++) {
+    lines.push({
+      x1: 0,
+      y1: random(200, windowHeight),
+      x2: width,
+      y2: random(windowHeight),
+      thickness: random(3, 16),
+    });
+  }
 }
 
 function draw() {
-  background(255, 255, 250);
+  background(255, 255, 245);
+  drawRectangle();
+  lines.forEach(line => {
+    drawLine(line);
+  });
+
   if (!isDone && frameCount > 100) {
     opacity_top-=5;
     opacity_bot+=5;
     if (frameCount %5 == 0) gap++;
   }
   if (opacity_top == 0) isDone = true;
+
+  drawText();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  lines.forEach(line => {
+    line.x2 = width;
+  });
+
+}
+
+function drawRectangle() {
+  noStroke();
+  fill(255, 111, 97);
+  triangle(width, -height/3, width, height, width/3, height);
+}
+
+function drawLine(l) {
+  noStroke();
+  stroke(247, 224, 87);
+  strokeWeight(l.thickness);
+  line(l.x1, l.y1, l.x2, l.y2);
+}
+
+function drawText() {
   noStroke();
   textSize(32);
-  fill(51, 51, 51, opacity_top);
+  fill(69, 181, 170, opacity_top);
   text("julien", 50, 60);
-  fill(51, 51, 51);
+  fill(69, 181, 170);
   text("la", 50, 100);
   text("forge", 50 + gap, 100);
-  fill(51, 51, 51, opacity_bot);
+  fill(69, 181, 170, opacity_bot);
   text("du web", 50, 140);
 }
